e935d545cb5eda23e8a124c283c0e44e53cc83d3
Hariharankr23 <krhariharan@nmsworks.co.in>
2024-08-07 17:03:35 +0530

    Coding exercise 1 completed

---
+++ hariharan/src/main/java/in/co/nmsworks/week1/Question3.java
@@ -0,0 +1,13 @@
+package in.co.nmsworks.week1;
+
+
+// program to print multiples of 7
+
+public class Question3 {
+    public static void main(String[] args) {
+
+      for(int i = 1 ; i <= 10 ;i++){
+          System.out.println(7 + "x" + i + " = "+ i*7);
+      }
+    }
+}
