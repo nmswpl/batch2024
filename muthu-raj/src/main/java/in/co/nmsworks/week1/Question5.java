070e30812265dde30936e6d40450360cb08c3a95
Muthu Raj <smuthuraj@nms.co.in>
2024-08-07 17:34:50 +0530

    updated Question5

--- muthu-raj/src/main/java/in/co/nmsworks/week1/Question5.java
+++ muthu-raj/src/main/java/in/co/nmsworks/week1/Question5.java
@@ -19,29 +19,16 @@ public class Question5 {
         System.out.println("Enter num3:: ");
         int num3 = sc.nextInt();
 
-        if (num1 < num2 && num1 > num3) {
-            System.out.println(num2);
-        } else if (num2 > num1 && num2 > num3) {
-            System.out.println(num2);
-        } else {
-            System.out.println(num3);
+        if (num1 > num2 && num1 > num3)
+        {
+            System.out.println(num1 +" is the greatest number");
         }
-
-        if (num2 > num3 && num2 > num1) {
-            System.out.println(num1);
-        } else if (num3 > num2 && num3 > num1) {
-            System.out.println(num3);
-        } else {
-            System.out.println(num2);
+        else if(num2 > num3)
+        {
+            System.out.println(num2 +" is the greatest number");
         }
-
-        if (num3 < num1 && num3 > num2) {
-            System.out.println(num3);
-        } else if (num1 < num3 && num1 > num2) {
-            System.out.println(num1);
-        } else {
-            System.out.println(num3);
-        }
+        else
+            System.out.println(num3 +" is the greatest number");
     }
 }
 
