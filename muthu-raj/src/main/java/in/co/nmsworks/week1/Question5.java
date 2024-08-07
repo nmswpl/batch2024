9605318431c0170d7cf94ecfd42b4ebabde18320
Muthu Raj <smuthuraj@nms.co.in>
2024-08-07 17:06:41 +0530

    finished coding exercise

--- muthu-raj/src/main/java/in/co/nmsworks/week1/Question5.java
+++ muthu-raj/src/main/java/in/co/nmsworks/week1/Question5.java
@@ -19,32 +19,31 @@ public class Question5 {
         System.out.println("Enter num3:: ");
         int num3 = sc.nextInt();
 
-//        num1
+        if (num1 < num2 && num1 > num3) {
+            System.out.println(num2);
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println(num2);
+        } else {
+            System.out.println(num3);
+        }
 
-        if (num1 > num2 && num1 > num3) {
-            System.out.println(num1 + " is the greatest number");
-        } else if (num1 == num2 && num1 > num3) {
-            System.out.println(num1 + " is the greatest number");
-        } else if (num1 == num3 && num1 > num2) {
-            System.out.println(num1 + " is the greatest number");
+        if (num2 > num3 && num2 > num1) {
+            System.out.println(num1);
+        } else if (num3 > num2 && num3 > num1) {
+            System.out.println(num3);
+        } else {
+            System.out.println(num2);
         }
-//num2
-        if (num2 > num1 && num2 > num3) {
-            System.out.println(num2 + " is the greatest number");
-        } else if (num2 == num1 && num2 > num3) {
-            System.out.println(num2 + " is the greatest number");
-        } else if (num2 == num3 && num2 > num1) {
-            System.out.println(num2 + " is the greatest number");
-        }
-//num3
-        if (num3 > num2 && num3 > num1) {
-            System.out.println(num3 + " is the greatest number");
-        } else if (num3 == num1 && num3 > num2) {
-            System.out.println(num3 + " is the greatest number");
-        } else if (num3 == num2 && num3 > num1) {
-            System.out.println(num3 + " is the greatest number");
+
+        if (num3 < num1 && num3 > num2) {
+            System.out.println(num3);
+        } else if (num1 < num3 && num1 > num2) {
+            System.out.println(num1);
+        } else {
+            System.out.println(num3);
         }
     }
 }
+
 
 
