156a148e7aa5efc61160f8b0aae2ffa6b14ef95c
PriyadharshiniG <gpriyadharshini@nmsworks.co.in>
2024-08-12 11:48:04 +0530

    Training excercise2 completed successfully.

---
+++ priya/src/main/java/in/co/nmsworks/week2/Te17.java
@@ -0,0 +1,15 @@
+package in.co.nmsworks.week2;
+
+public class Te17 {
+    public void maxArray(int[] arr){
+      int max = arr[0];
+        for (int i = 1; i <arr.length ; i++) {
+            if(arr[i]>=max){
+                max = arr[i];
+            }
+
+        }
+        System.out.println(max);
+
+    }
+}
