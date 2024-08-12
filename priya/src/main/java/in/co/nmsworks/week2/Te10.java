9b8e1a057ea8e89385dd68ecd2212bd974ea1a92
PriyadharshiniG <gpriyadharshini@nmsworks.co.in>
2024-08-12 11:41:45 +0530

    Training excercise2 completed successfully.

---
+++ priya/src/main/java/in/co/nmsworks/week2/Te10.java
@@ -0,0 +1,12 @@
+package in.co.nmsworks.week2;
+
+public class Te10 {
+    public void getNumbers(int start,int end){
+        for (int i = start; i <end ; i++) {
+            if(i%10==7) {
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+}
