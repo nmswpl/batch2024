332b1ba4326d0a0c760a9495483c51ba982f7003
PriyadharshiniG <gpriyadharshini@nmsworks.co.in>
2024-08-19 19:02:41 +0530

    Week 3 assessment 3rd and 4th question updated.

--- priya/src/main/java/in/co/nmsworks/week3/assessment/Question3.java
+++ priya/src/main/java/in/co/nmsworks/week3/assessment/Question3.java
@@ -1,37 +1,58 @@
 package in.co.nmsworks.week3.assessment;
 /*3) Write a program to find if a given number (less than 1000) is prime or not using Set.*/
+
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-
+/*3) Write a program to find if a given number (less than 1000) is prime or not using Set.*/
 public class Question3 {
-    public static void main(String[] args) {
-        Set<Integer> prime = new HashSet<>();
-        Scanner s = new Scanner(System.in);
-        prime.add(2);
-        for (int i = 1; i <1000 ; i++) {
-            prime.add(i);
-        }
-        for (int i = 3; i <1000 ; i++) {
+    Set<Integer> prime = new HashSet<>();
 
-            for (int j = 2; j <i; j++) {
-                if (i%j== 0) {
-                    prime.remove(i);
-                }
+    public boolean isPrime(int num) {
+        int count = 0;
+        for (int i = 1; i <= num/2; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+
+        }
+        return count == 1;
+    }
+
+   public void addPrime() {
+       for (int i = 1; i <= 1000; i++) {
+            if (isPrime(i)) {
+                prime.add(i);
             }
         }
-        while(true){
-            s.nextInt();
-            if(s.nextInt() ==-1){
+    }
+
+    public void checkPrime() {
+       addPrime();
+
+
+        Scanner s = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter a number:");
+            int value = s.nextInt();
+            if (value == -1) {
                 break;
             }
-            if(prime.contains(s.nextInt())){
+            if (prime.contains(value)) {
                 System.out.println("prime");
+            } else {
+                System.out.println("not prime");
             }
         }
-
 
 
     }
+
+    public static void main(String[] args) {
+        Question3 q3 = new Question3();
+        q3.checkPrime();
+    }
+
 }
 
--- priya/src/main/java/in/co/nmsworks/week3/assessment/Tutioncenter.java
+++ priya/src/main/java/in/co/nmsworks/week3/assessment/Tutioncenter.java
@@ -1,15 +1,94 @@
-/*
 package in.co.nmsworks.week3.assessment;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Tutioncenter {
+    public  Tutioncenter(){
+
+    }
+
+    public Tutioncenter(String branchname, String branchCode) {
+        this.branchname = branchname;
+        this.branchCode = branchCode;
+    }
+
+    private String branchname;
+    private String branchCode;
+    private   Map<String,Integer> subToStudentCount = new HashMap<>();
+
+    public Map<String, Integer> getSubToStudentCount() {
+        return subToStudentCount;
+    }
+
+    public void setSubToStudentCount(Map<String, Integer> subToStudentCount) {
+        this.subToStudentCount = subToStudentCount;
+    }
+
+    public String getBranchname() {
+        return branchname;
+    }
+
+    public void setBranchname(String branchname) {
+        this.branchname = branchname;
+    }
+
+    public String getBranchCode() {
+        return branchCode;
+    }
+
+    public void setBranchCode(String branchCode) {
+        this.branchCode = branchCode;
+    }
+    public Set<String> getSubjects(){
+        return subToStudentCount.keySet();
+    }
+    public  int getTotalStudents(){
+        int totalStudents =0;
+        for(Integer value : subToStudentCount.values()){
+            totalStudents+=value;
+        }
+        return totalStudents;
+    }
+
+    @Override
+    public String toString() {
+        return "Tutioncenter{" +
+                "branchname='" + branchname + '\'' +
+                ", branchCode='" + branchCode + '\'' +
+                ", subToStudentCount=" + subToStudentCount +
+                '}';
+    }
+
     public static void main(String[] args) {
-        Map<String, List<Map<String,Integer>>> TutionCentre = new HashMap<>();
-        Map<String,Integer> subToStudents<>
+        Tutioncenter chennaicenter = new Tutioncenter("Chennai","CH01");
+        Tutioncenter salemcenter = new Tutioncenter("Salem","SA01");
+        Tutioncenter maduraicenter = new Tutioncenter("Madurai","MD01");
+        chennaicenter.subToStudentCount.put("Physics",30);
+        chennaicenter.subToStudentCount.put("Chemistry",40);
+        chennaicenter.subToStudentCount.put("Biology",50);
+        chennaicenter.subToStudentCount.put("Maths",40);
+        chennaicenter.subToStudentCount.put("Hindi",20);
+        salemcenter.subToStudentCount.put("Physics",25);
+        salemcenter.subToStudentCount.put("Chemistry",35);
+        salemcenter.subToStudentCount.put("Biology",30);
+        salemcenter.subToStudentCount.put("Maths",40);
+        maduraicenter.subToStudentCount.put("Physics",20);
+        maduraicenter.subToStudentCount.put("Chemistry",30);
+        maduraicenter.subToStudentCount.put("Biology",40);
+        maduraicenter.subToStudentCount.put("Maths",30);
+        System.out.println(chennaicenter);
+        System.out.println(salemcenter);
+        System.out.println(maduraicenter);
+        System.out.println(chennaicenter.getSubjects());
+        System.out.println(maduraicenter.getSubjects());
+        System.out.println(salemcenter.getSubjects());
+        System.out.println( chennaicenter.getTotalStudents());
+        System.out.println( salemcenter.getTotalStudents());
+        System.out.println( maduraicenter.getTotalStudents());
+
+
 
     }
 }
-*/
+
