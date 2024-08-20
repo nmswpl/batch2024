944a6916e5d5b02d4f2193aa8da10ea9e41cef48
Sujaritha <vsujaritha@nmsworks.co.in>
2024-08-20 12:59:31 +0530

    Week3 Day2 Exercises updated

--- suji/src/main/java/in/co/nmsworks/week3/File5.java
+++ suji/src/main/java/in/co/nmsworks/week3/File5.java
@@ -4,19 +4,19 @@ import java.io.*;
 
 public class File5 {
     public static void main(String[] args)throws IOException {
-        FileReader fr=new FileReader("/home/nms/Documents/input.txt");
+      /*  FileReader fr=new FileReader("/home/nms/Documents/input.txt");
         BufferedReader br=new BufferedReader(fr);
 
         if (br.read()==-1) {
 
             br.close();
             fr.close();
-        }
+        } */
             FileWriter fw = new FileWriter("/home/nms/Documents/input.txt",true);
             BufferedWriter bw = new BufferedWriter(fw);
 
             bw.newLine();
-            bw.append("Appending,new,string");
+            bw.append("Appending new content");
 
         bw.close();
         fw.close();
