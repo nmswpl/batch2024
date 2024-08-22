feae81d4945332f42c07c4cb182cfbadc07540f5
Aarthi <raarthi@nmsworks.co.in>
2024-08-23 00:16:46 +0530

    Day 4 Exercise completed successfully

---
+++ aarthi/src/main/java/in/co/nmsworks/week2/collection/Pq14.java
@@ -0,0 +1,24 @@
+package in.co.nmsworks.week2.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pq14
+{
+
+    public void testlist()
+    {
+        Integer[] arr=new Integer[3];
+        List<Integer[]>lists=new ArrayList<>();
+        lists.add(new Integer[]{7,8,9});
+        lists.add(new Integer[]{8,9,6});
+        lists.add(new Integer[]{12,23,24,56});
+        System.out.println(lists);
+
+
+
+
+
+
+    }
+}
