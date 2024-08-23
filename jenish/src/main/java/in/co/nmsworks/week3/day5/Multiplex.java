c506b28b793cdd3b31eb66be78e8efe9af06dfdc
Jenish <rjenish@nmsworks.co.in>
2024-08-23 13:50:15 +0530

    Day 5 Practice problems Completed

--- jenish/src/main/java/in/co/nmsworks/week3/day5/Multiplex.java
+++ jenish/src/main/java/in/co/nmsworks/week3/day5/Multiplex.java
@@ -42,124 +42,99 @@ public class Multiplex {
         this.ticketCountScreen3 = ticketCountScreen3;
     }
 
-
-    public void ticketsCounter(){
-        for (int i = 1; i <= 50 ; i++) {
-            this.ticketCountScreen1.add(i);
+    public void ticketsCounter() {
+        for (int i = 1; i <= 50; i++) {
+            ticketCountScreen1.add(i);
         }
-        for (int i = 1; i <= 75 ; i++) {
-            this.ticketCountScreen2.add(i);
+        for (int i = 1; i <= 75; i++) {
+            ticketCountScreen2.add(i);
         }
-        for (int i = 1; i <= 40 ; i++) {
-            this.ticketCountScreen3.add(i);
+        for (int i = 1; i <= 40; i++) {
+            ticketCountScreen3.add(i);
         }
     }
 
-    public void bookTicket(String screenName, int noOfTickets){
-        if(screenName.equalsIgnoreCase("screen1")) {
-            if (noOfTickets <= ticketCountScreen1.size()) {
-                List<Integer> bookedTickets = new ArrayList<>();
-                for (int i = ticketCountScreen1.get(0); i <= noOfTickets; i++) {
-                    int bookedSeats = ticketCountScreen1.remove(i);
-                    bookedTickets.add(bookedSeats);
-                }
-            System.out.println(ticketCountScreen1);
-            System.out.println("Ticket numbers are : " + bookedTickets);
+    public void bookTicket(String screenName, int noOfTickets) {
+        List<Integer> ticketList;
+        if (screenName.equalsIgnoreCase("screen1")) {
+            ticketList = ticketCountScreen1;
+        } else if (screenName.equalsIgnoreCase("screen2")) {
+            ticketList = ticketCountScreen2;
+        } else if (screenName.equalsIgnoreCase("screen3")) {
+            ticketList = ticketCountScreen3;
+        } else {
+            System.out.println("Invalid screen name.");
+            return;
+        }
+
+        if (noOfTickets <= ticketList.size()) {
+            List<Integer> bookedTickets = new ArrayList<>();
+            for (int i = 0; i < noOfTickets; i++) {
+                bookedTickets.add(ticketList.remove(0));
+            }
+            System.out.println("Available tickets: " + ticketList);
+            System.out.println("Ticket numbers are: " + bookedTickets);
             System.out.println(noOfTickets + " Tickets booked");
-            }
-            else {
-                System.out.println("Tickets unavailable!!");
-            }
-        }
-        else if(screenName.equalsIgnoreCase("screen2")){
-            if(noOfTickets <= ticketCountScreen2.size()) {
-                List<Integer> bookedTickets = new ArrayList<>();
-                for (int i = ticketCountScreen2.get(0); i <= noOfTickets; i++) {
-                    int bookedSeats = ticketCountScreen2.remove(i);
-                    bookedTickets.add(bookedSeats);
-                }
-                System.out.println(ticketCountScreen2);
-                System.out.println("Ticket numbers are : " + bookedTickets);
-                System.out.println(noOfTickets + " Tickets booked");
-            }
-            else {
-                System.out.println("Tickets unavailable!!");
-            }
-        }
-        else if(screenName.equalsIgnoreCase("screen3")){
-            if(noOfTickets <= ticketCountScreen3.size()) {
-                List<Integer> bookedTickets = new ArrayList<>();
-                for (int i = ticketCountScreen3.get(0); i <= noOfTickets; i++) {
-                    int bookedSeats = ticketCountScreen3.remove(i);
-                    bookedTickets.add(bookedSeats);
-                }
-                System.out.println(ticketCountScreen3);
-                System.out.println("Ticket numbers are : " + bookedTickets);
-                System.out.println(noOfTickets + " Tickets booked");
-            }
-            else {
-                System.out.println("Tickets unavailable!!");
-            }
+        } else {
+            System.out.println("Tickets unavailable!!");
         }
     }
 
     public void showAvailability(String screenName) {
+        List<Integer> ticketList;
         if (screenName.equalsIgnoreCase("screen1")) {
-            int availableTickets = ticketCountScreen1.size();
-            System.out.println("Available tickets in screen1: " + availableTickets);
+            ticketList = ticketCountScreen1;
         } else if (screenName.equalsIgnoreCase("screen2")) {
-            int availableTickets = ticketCountScreen2.size();
-            System.out.println("Available tickets in screen2: " + availableTickets);
+            ticketList = ticketCountScreen2;
         } else if (screenName.equalsIgnoreCase("screen3")) {
-            int availableTickets = ticketCountScreen3.size();
-            System.out.println("Available tickets in screen3: " + availableTickets);
+            ticketList = ticketCountScreen3;
+        } else {
+            System.out.println("Invalid screen name.");
+            return;
         }
+        int availableTickets = ticketList.size();
+        System.out.println("Available tickets in " + screenName + ": " + availableTickets);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Multiplex mul = new Multiplex();
         mul.ticketsCounter();
         Scanner scanner = new Scanner(System.in);
-        while (true)
-        {
+        while (true) {
             System.out.println("\n***********************************************\n Welcome to Multiplex");
             System.out.println("1. Book Ticket");
             System.out.println("2. Check Availability");
             System.out.println("3. Quit \n");
             System.out.print("Enter Your Choice (1/2/3) :: ");
             int choice = scanner.nextInt();
-            switch (choice)
-            {
+            switch (choice) {
                 case 1:
                     System.out.print("\n\nChoose Screen \n\t 1. screen1\n\t 2. screen2\n\t 3. screen3\nEnter Choice (1/2/3) :: ");
                     int screenNo = scanner.nextInt();
-                    if (screenNo < 1 || screenNo > 3)
-                    {
+                    if (screenNo < 1 || screenNo > 3) {
                         System.err.println("Invalid screen number.");
                         break;
                     }
-                    System.out.print("Enter No Of Tickets ::");
+                    System.out.print("Enter No Of Tickets :: ");
                     int noOfTickets = scanner.nextInt();
-                    mul.bookTicket("screen"+screenNo, noOfTickets);
+                    mul.bookTicket("screen" + screenNo, noOfTickets);
                     break;
                 case 2:
                     System.out.print("\n\nChoose Screen \n\t 1. screen1\n\t 2. screen2\n\t 3. screen3\nEnter Choice (1/2/3) :: ");
                     screenNo = scanner.nextInt();
-                    if (screenNo < 1 || screenNo > 3)
-                    {
-                        System.out.println("Invalid screen number.");
+                    if (screenNo < 1 || screenNo > 3) {
+                        System.err.println("Invalid screen number.");
                         break;
                     }
-                    mul.showAvailability("screen"+screenNo);
+                    mul.showAvailability("screen" + screenNo);
                     break;
                 case 3:
                     System.out.println("Thank You for visiting Multiplex");
+                    scanner.close();
                     return;
                 default:
                     System.out.println("Invalid Option !!");
             }
         }
     }
-
 }
