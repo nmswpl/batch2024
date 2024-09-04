a52858efd0c30641ca4df99b1921ee0b960c7e9a
Jenish R <jenjenish747@gmail.com>
2024-09-04 22:20:22 +0530

    week 5 practice example question completed

---
+++ jenish/src/main/java/in/co/nmsworks/week5/UserManager.java
@@ -0,0 +1,49 @@
+package in.co.nmsworks.week5;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+public class UserManager {
+    public void getAllUsers()
+    {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet request = new HttpGet("https://jsonplaceholder.typicode.com/users");
+
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                int statusCode = response.getStatusLine().getStatusCode();
+                System.out.println("Status Code: " + statusCode);
+
+                String responseBody = EntityUtils.toString(response.getEntity());
+                //System.out.println("Response Body: " + responseBody);
+
+                Gson gson = new Gson();
+                List<User> users = gson.fromJson(responseBody, new TypeToken<List<User>>(){}.getType());
+
+                for (User user : users) {
+                    System.out.println(user);
+                }
+
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        UserManager userManager = new UserManager();
+        userManager.getAllUsers();
+    }
+}
